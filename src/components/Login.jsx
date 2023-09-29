4c7711e616b124b902aba450518dfa9bd551d801
sbruinsma1 <sbruinsma@berkeley.edu>
2023-09-29 11:36:44 -0400

    updated Login.jsx code in line with prettier demands

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,11 +1,11 @@
-import React, { useState, useEffect } from "react";
-import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
+import React, { useState, useEffect } from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 function Login({ onLogin, envParticipantId, envStudyId, validationFunction }) {
   // State variables for login screen
-  const [participantId, setParticipant] = useState("");
-  const [studyId, setStudy] = useState("");
+  const [participantId, setParticipant] = useState('');
+  const [studyId, setStudy] = useState('');
   const [error, setError] = useState(false);
 
   useEffect(() => {
@@ -24,47 +24,47 @@ function Login({ onLogin, envParticipantId, envStudyId, validationFunction }) {
     e.preventDefault();
     // Validates fields
     validationFunction(participantId, studyId)
-    // Logs in depending on result from promise
-    .then((loggedIn) => {
-      if (loggedIn) {
-        onLogin(loggedIn, studyId, participantId);
-      } else {
-        setError(true);
-      }
-    });
+      // Logs in depending on result from promise
+      .then((loggedIn) => {
+        if (loggedIn) {
+          onLogin(loggedIn, studyId, participantId);
+        } else {
+          setError(true);
+        }
+      });
   }
 
   return (
-    <div className="centered-h-v">
-      <div className="width-50">
+    <div className='centered-h-v'>
+      <div className='width-50'>
         {error ? (
-          <div className="alert alert-danger" role="alert">
+          <div className='alert alert-danger' role='alert'>
             The participant ID and study ID do not match
           </div>
         ) : null}
-        <Form className="centered-h-v" onSubmit={handleSubmit}>
-          <Form.Group className="width-100" size="lg" controlId="participantId">
+        <Form className='centered-h-v' onSubmit={handleSubmit}>
+          <Form.Group className='width-100' size='lg' controlId='participantId'>
             <Form.Label>Participant ID</Form.Label>
             <Form.Control
               autoFocus
-              type="participantId"
+              type='participantId'
               value={participantId}
               onChange={(e) => setParticipant(e.target.value)}
             />
           </Form.Group>
-          <Form.Group className="width-100" size="lg" controlId="studyId">
+          <Form.Group className='width-100' size='lg' controlId='studyId'>
             <Form.Label>Study ID</Form.Label>
             <Form.Control
-              type="studyId"
+              type='studyId'
               value={studyId}
               onChange={(e) => setStudy(e.target.value)}
             />
           </Form.Group>
           <Button
-            style={{ width: "100%" }}
+            style={{ width: '100%' }}
             block
-            size="lg"
-            type="submit"
+            size='lg'
+            type='submit'
             disabled={!validateForm()}
           >
             Log In
