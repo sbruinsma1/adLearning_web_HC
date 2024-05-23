c5fd721ee092012c104fa98e37010ee2e8e3ee56
sbruinsma1 <55825532+sbruinsma1@users.noreply.github.com>
2024-05-22 20:11:18 -0400

    clean for action deployment

--- src/js/blocksetting123.js
+++ src/js/blocksetting123.js
@@ -20,7 +20,7 @@ import { images } from '../lib/utils';
 
 // design
 const n_TrialPerBlock = 200; 
-const n_TrialPractice = 30;
+const n_TrialPractice = 30; 
 const n_SamePosition = 7;
 const n_MaxJitter = 4; // 7-11, avg of 9
 const rtDeadline = 15000;
